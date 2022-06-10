bd79502dbf3e559479088fe5492983fb095c8072
Philippe Roy <104743177+PhilippeRoy5@users.noreply.github.com>
2022-06-10 16:59:39 -0400

    Update Cyclistic.sql

--- Cyclistic.sql
+++ Cyclistic.sql
@@ -54,7 +54,7 @@ FROM [cycle_DB].[dbo].['202204']
 ) a
 
 
--- ----------------------Cleaning out the "NULL" from the table------------------------------------------
+-------------------------------------------Cleaning out the "NULL" from the table------------------------------------------
 
 
 DELETE 
@@ -82,7 +82,7 @@ SET week_day = CASE
 	END
 
 
--- ------------------------------------------Adding the Month names into a new column ------------------------------------------
+---------------------------------------------Adding the Month names into a new column ------------------------------------------
 
 
 ALTER TABLE [cycle_DB].[dbo].[Year_Table]
@@ -112,7 +112,7 @@ ALTER TABLE [cycle_DB].[dbo].[Year_Table]
 DROP COLUMN day_of_week, month
 
 
--- ------------------------------------------Cleaning ride ID ------------------------------------------
+-------------------------------------------------Cleaning ride ID ----------------------------------------------------------
 
 
 DELETE
@@ -122,7 +122,7 @@ WHERE LEN(ride_id) <> 16
 
 
 
--- ---------------------Removing the maintenance Stations from the Start Stations------------------------------------------
+--------------------------Removing the maintenance Stations from the Start Stations------------------------------------------
 
 
 DELETE 
@@ -131,7 +131,7 @@ WHERE start_station_name LIKE '%TEST%'
 AND start_station_name LIKE '%REPAIR%'
 
 
--- ----------------------------Removing the maintenance Stations from the End Stations------------------------------------------
+--------------------------------Removing the maintenance Stations from the End Stations-----------------------------------------
 
 
 DELETE 
@@ -141,7 +141,7 @@ AND end_station_name LIKE '%REPAIR%'
 
 
 
---------------------------------------------Cleaning Start Station names------------------------------------------
+------------------------------------------------Cleaning Start Station names------------------------------------------------------
 
 ALTER TABLE [cycle_DB].[dbo].[Year_Table]
 ADD clean_start_station Nvarchar(255);
@@ -152,7 +152,7 @@ SET clean_start_station = TRIM(
 
 
 
---------------------------------------------Cleaning End Station names------------------------------------------
+-------------------------------------------------Cleaning End Station names----------------------------------------------------------
 
 
 ALTER TABLE [cycle_DB].[dbo].[Year_Table]
@@ -172,7 +172,7 @@ ALTER TABLE [cycle_DB].[dbo].[Year_Table]
 DROP COLUMN start_station_name, end_station_name
 
 
---------------------------------------------Creating the Final Table for exploration------------------------------------------
+--------------------------------------------Creating the Final Table for exploration---------------------------------------------
 
 SELECT *
 INTO [cycle_DB].[dbo].[Final_table]
@@ -181,7 +181,7 @@ INTO [cycle_DB].[dbo].[Final_table]
 	month_year,clean_start_station,clean_end_station, CAST(crid.total_time as time) as ride_length
 	FROM [cycle_DB].[dbo].[Year_Table]
 
---------------------------------------------Removing ride length under 1 minute------------------------------------------
+--------------------------------------------Removing ride length under 1 minute--------------------------------------------------
 
 DELETE
 FROM [cycle_DB].[dbo].[Final_table]
@@ -190,13 +190,13 @@ WHERE ride_length < '00:01:00'
 
 
 
--------------------------/****** Creating the Geo locations table  ******/---------------------------------------------
+--------------------------------/****** Creating the Geo locations table  ******/--------------------------------------------------
 
 		
 /** Departing station table **/
 
 
-----------Fixing coordinates for casual rider Start_station by creating a casual_dep table---------------------------
+----------------------------Fixing coordinates for casual rider Start_station by creating a casual_dep table------------------------
 
 
 WITH casual_dep AS (
@@ -208,7 +208,7 @@ WITH casual_dep AS (
  GROUP BY clean_start_station
  ),
 
- -------------Fixing coordinates for member Start_station by creating a member_dep table---------------
+ ---------------------------Fixing coordinates for member Start_station by creating a member_dep table-----------------------------
 
  member_dep AS(
 
@@ -219,7 +219,7 @@ WITH casual_dep AS (
  GROUP BY clean_start_station
  ),
 
- --------Joining Depart station for member and casual together and exporting to CSV file-------------
+ ----------------------------Joining Depart station for member and casual together and exporting to CSV file--------------------------
 
 Depart_station_viz AS (
 
@@ -232,7 +232,7 @@ Depart_station_viz AS (
 
  /****** Arriving station table  ******/
 
---------------Fixing coordinates for member End_station by creating a member_arr table-------------------
+-----------------------------Fixing coordinates for member End_station by creating a member_arr table------------------------------------
 
 WITH member_arr AS(
 
@@ -243,7 +243,7 @@ SELECT distinct(clean_end_station), ROUND(AVG(cast(end_lat as float)),4) as arr_
  GROUP BY clean_end_station
  ),
 
- -- Fixing coordinates for casual rider End_station by creating a casual_arr table
+ ------------------Fixing coordinates for casual rider End_station by creating a casual_arr table--------------------------
 
 casual_arr AS(
 SELECT distinct(clean_end_station), ROUND(AVG(cast(end_lat as float)),4) as arr_lat,
@@ -287,7 +287,7 @@ WHERE member_casual = 'casual'
 GROUP BY month_year
 
 
-------------------------Calculation the amount of rides on different days per rider type --------------------------------------
+------------------------Calculation the amount of rides on different days per rider type ---------------------------
 
 
 SELECT week_day, member_casual, count(ride_id) as rides
@@ -310,7 +310,7 @@ WHERE rideable_type <> 'docked_bike'
 GROUP BY rideable_type, member_casual
 
 
----------------------- Calculating the average ride length per rider type ------------------------------------------
+---------------------- Calculating the average ride length per rider type ----------------------------------------
 
 SELECT member_casual, AVG(ride_length) as AVG_length
 FROM [cycle_DB].[dbo].[Final_table]
